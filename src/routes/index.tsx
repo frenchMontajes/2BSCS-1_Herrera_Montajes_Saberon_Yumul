72bccebfded0599dca2ca54f1134dc81d483b4a8
frenchMontajes <frenchoymontajess@gmail.com>
2024-05-06 19:05:28 +0800

    new ui

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,6 @@ import { Navbar } from "../components/navbar";
 import { Hero } from "../components/hero";
 //import {}
 
-
 //hompage
 export const Route = createFileRoute("/")({
   component: Component,
